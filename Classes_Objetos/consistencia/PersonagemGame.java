03959b6892c1e3c3d578359b27921c018a1dd6da
Gabriel-Rocha-Gomes <g.rocha.g8@gmail.com>
2025-07-15 09:50:10 -0300

    Java - Classes e Objetos - Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,59 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.setSaudeAtual(saudeAtual);
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.isEmpty() && nome != null ) {
+            this.nome = nome;
+        }
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual == 0) {
+            this.setStatus("morto");
+        } else if (this.saudeAtual > 0) {
+            this.setStatus("vivo");
+        }
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        int saude = this.saudeAtual -= quantidadeDeDano;
+        if (saude < 0) {
+            this.setSaudeAtual(0);
+        } else {
+            this.setSaudeAtual(saude);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int saude = this.saudeAtual += quantidadeDeCura;
+        if (saude > 100) {
+            this.setSaudeAtual(100);
+        } else {
+            this.setSaudeAtual(saude);
+        }
+    }
+}
