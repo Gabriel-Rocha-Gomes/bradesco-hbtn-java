5e7d152527f113980df0f0eea150684d40430429
Gabriel-Rocha-Gomes <g.rocha.g8@gmail.com>
2025-07-15 09:32:22 -0300

    Java - Classes e Objetos - 3. Status do Personagem

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual == 0) {
+            this.setStatus("Morto");
+        } else if (this.saudeAtual > 0) {
+            this.setStatus("Vivo");
+        }
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        int saude = this.saudeAtual -= quantidadeDeDano;
+        if (saude < 0) {
+            this.setSaudeAtual(0);
+        } else {
+            this.setSaudeAtual(saude);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int saude = this.saudeAtual += quantidadeDeCura;
+        if (saude > 100) {
+            this.setSaudeAtual(100);
+        } else {
+            this.setSaudeAtual(saude);
+        }
+    }
+}
