719b94e33214bc3af80cd06512e359b3dbf19e34
Gabriel-Rocha-Gomes <g.rocha.g8@gmail.com>
2025-07-17 10:39:22 -0300

    Herança 3. Quadrado

---
+++ Heranca/quadrado/FormaGeometrica.java
@@ -0,0 +1,8 @@
+package Heranca.quadrado;
+
+public class FormaGeometrica {
+    
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
