080e2dbd06ca9da8b3bdfc68a797886e3cafc554
Gabriel-Rocha-Gomes <g.rocha.g8@gmail.com>
2025-07-17 09:07:57 -0300

    Herança 0. Herança

--- Heranca/heranca/Program.java
+++ Heranca/heranca/Program.java
@@ -1,15 +1,13 @@
-package Heranca.heranca;
+public class Program {
+    public static void main(String[] args) {
+        Animal meuAnimal = new Animal();
+        Cachorro meuCachorro = new Cachorro();
+        Object obj = meuCachorro;
+        Animal umOutroAnimal = meuCachorro;
 
-   public class Program {
-        public static void main(String[] args) {
-            Animal meuAnimal = new Animal();
-            Cachorro meuCachorro = new Cachorro();
-            Object obj = meuCachorro;
-            Animal umOutroAnimal = meuCachorro;
-
-            System.out.println("meuAnimal eh um " + meuAnimal.getClass().getSimpleName());
-            System.out.println("meuCachorro eh um " + meuCachorro.getClass().getSimpleName());
-            System.out.println("obj eh um " + obj.getClass().getSimpleName());
-            System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getSimpleName());
-        }
+        System.out.println("meuAnimal eh um " + meuAnimal.getClass().getSimpleName());
+        System.out.println("meuCachorro eh um " + meuCachorro.getClass().getSimpleName());
+        System.out.println("obj eh um " + obj.getClass().getSimpleName());
+        System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getSimpleName());
     }
+}
