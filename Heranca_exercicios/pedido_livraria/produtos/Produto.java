47522a5a35da6105e7609fe70ce4fad446a20269
Gabriel-Rocha-Gomes <g.rocha.g8@gmail.com>
2025-07-31 17:58:18 -0300

    Delete Heranca_exercicios directory

--- Heranca_exercicios/pedido_livraria/produtos/Produto.java
+++
@@ -1,8 +0,0 @@
-public abstract class Produto {
-    protected String titulo;
-    protected int ano;
-    protected String pais;
-    protected double precoBruto;
-
-    public abstract double obterPrecoLiquido();
-}
